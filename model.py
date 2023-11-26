2dde26fbfc21f91a021113bf5b5c562e29b2586c
NolanRobins <56159640+NolanRobins@users.noreply.github.com>
2023-11-26 11:06:15 -0500

    Update model.py

--- model.py
+++ model.py
@@ -6,170 +6,217 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
 import keras_cv
 import PIL
+from PIL import Image, ImageDraw, ImageFont
 
 BATCH_SIZE = 32
 
-while True:
-    if(tf.config.list_physical_devices('GPU') == []):
-        response = input("\033[93mGPU NOT DETECTED AND WILL NOT BE USED.\nPERFORMANCE MAY BE REDUCED. CONTINUE ANYWAYS?\033[0m \033[96m Y/[N]:\033[0m ")
-        if response == 'y' or response == 'Y':
-            break
-        elif response == 'n' or response == 'N' or response == '':
-            exit()
+def main():
+    while True:
+        if(tf.config.list_physical_devices('GPU') == []):
+            response = input("\033[93mGPU NOT DETECTED AND WILL NOT BE USED.\nPERFORMANCE MAY BE REDUCED. CONTINUE ANYWAYS?\033[0m \033[96m Y/[N]:\033[0m ")
+            if response == 'y' or response == 'Y':
+                break
+            elif response == 'n' or response == 'N' or response == '':
+                exit()
+            else:
+                print("Input Not Valid")
         else:
-            print("Input Not Valid")
-    else:
-        break
+            break
 
-train_preprocessor = tf.keras.Sequential([
-    layers.RandomTranslation(0, (-0.2,0.2), seed = 852),
-    keras_cv.layers.RandomShear(x_factor=0.2, y_factor=0.2, seed = 852),
-    layers.GaussianNoise(0.2, seed = 727),
-    layers.RandomZoom((-0.15, 0.15), (-0.15, 0.15), fill_mode = "constant", seed = 852),
-    # layers.RandomBrightness((0,0.5), seed = 852),
-    # keras_cv.layers.RandomSharpness((0.8, 0.8), (0, 255), seed = 852),
-    # keras_cv.layers.AutoContrast((0, 255)),
-    # layers.Resizing(100, 100),
-    layers.Rescaling(1./255),
-])
+    train_preprocessor = tf.keras.Sequential([
+        layers.RandomTranslation(0, (-0.2,0.2), seed = 852),
+        keras_cv.layers.RandomShear(x_factor=0.3, y_factor=0.3, seed = 852),
+        layers.GaussianNoise(0.3, seed = 727),
+        layers.RandomZoom((-0.15, 0.15), (-0.15, 0.15), fill_mode = "constant", seed = 852),
+        # layers.RandomBrightness((0,0.5), seed = 852),
+        # keras_cv.layers.RandomSharpness((0.8, 0.8), (0, 255), seed = 852),
+        # keras_cv.layers.AutoContrast((0, 255)),
+        # layers.Resizing(100, 100),
+        layers.Rescaling(1./255),
+    ])
 
-val_preprocessor = tf.keras.Sequential([
-    layers.Rescaling(1./255),
-])
+    val_preprocessor = tf.keras.Sequential([
+        layers.Rescaling(1./255),
+    ])
 
-train_set = tf.keras.utils.image_dataset_from_directory(
-    "Data/Train",
-    labels="inferred",
-    label_mode="categorical",
-    image_size=(100, 100),
-    color_mode="rgb",
-    batch_size = BATCH_SIZE,
-    seed = 852,
-    # interpolation = "nearest",
-)
+    train_set = tf.keras.utils.image_dataset_from_directory(
+        "Data/Train",
+        labels="inferred",
+        class_names=["Large", "Medium", "None", "Small"],
+        label_mode="categorical",
+        image_size=(100, 100),
+        color_mode="rgb",
+        batch_size = BATCH_SIZE,
+        seed = 852,
+        # interpolation = "nearest",
+    )
 
-class_names = train_set.class_names
+    print(train_set.class_names)
+    labels = train_set.class_names
 
-train_set = train_set.map(lambda x, y: (train_preprocessor(x, training = True), y))
+    train_set = train_set.map(lambda x, y: (train_preprocessor(x, training = True), y))
 
-val_set = tf.keras.utils.image_dataset_from_directory(
-    "Data/Validation",
-    labels="inferred",
-    label_mode="categorical",
-    image_size=(100,100),
-    color_mode="rgb",
-    batch_size = BATCH_SIZE,
-    seed = 852,
-)
+    val_set = tf.keras.utils.image_dataset_from_directory(
+        "Data/Validation",
+        labels="inferred",
+        label_mode="categorical",
+        image_size=(100,100),
+        color_mode="rgb",
+        batch_size = BATCH_SIZE,
+        seed = 852,
+    )
 
-val_set = val_set.map(lambda x, y: (val_preprocessor(x), y))
-
-
-for images, labels in train_set.take(1):
-    augmented_images = images.numpy()
+    val_set = val_set.map(lambda x, y: (val_preprocessor(x), y))
 
 
-plt.figure(figsize=(10, 10))
-for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(augmented_images[i])
-    plt.axis("off")
-plt.savefig("ModelOutput/datafig.png")
+    for images, labels in train_set.take(1):
+        augmented_images = images.numpy()
 
 
-for images, labels in val_set.take(1):
-    augmented_images = images.numpy()
-
-plt.figure(figsize=(10, 10))
-for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(augmented_images[i])
-    plt.axis("off")
-plt.savefig("ModelOutput/valfig.png")
+    plt.figure(figsize=(10, 10))
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(augmented_images[i])
+        plt.axis("off")
+    plt.savefig("ModelOutput/datafig.png")
 
 
-AUTOTUNE = tf.data.AUTOTUNE
+    for images, labels in val_set.take(1):
+        augmented_images = images.numpy()
 
-train_set = train_set.cache().shuffle(1600).prefetch(buffer_size=AUTOTUNE)
-
-
-model = keras.Sequential([
-    layers.Conv2D(256, 2, strides=(1, 1), activation= 'relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.2),
-
-    layers.Conv2D(64, 2, strides=(1, 1), activation= 'relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.5),
-
-    layers.Conv2D(64, 4, activation= 'leaky_relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.35),
-
-    layers.Conv2D(64, 3, activation= 'relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.5),
-
-    layers.Conv2D(64, 3, activation= 'relu'),
-    layers.MaxPooling2D(),
-   
-
-    layers.Flatten(),
-    layers.Dense(256, activation='elu'),
-    layers.Dropout(0.5),
-
-    layers.Dense(4, activation='softmax')
-])
-
-optimizer=keras.optimizers.Adam()
-optimizer.learning_rate.assign(0.0005)
-
-model.compile(optimizer=optimizer,
-              loss=tf.keras.losses.CategoricalCrossentropy(),
-              metrics=['accuracy'])
-
-model.build((BATCH_SIZE, 100, 100, 3))
-
-model.summary()
-
-# reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy', factor=0.2, patience=5, min_lr=0.00001)
-
-epochs = 50
-history = model.fit(
-  train_set,
-  validation_data=val_set,
-  epochs=epochs,
-#   callbacks = reduce_lr,
-)
-
-model.save("ModelOutput/modeled.keras")
+    plt.figure(figsize=(10, 10))
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(augmented_images[i])
+        plt.axis("off")
+    plt.savefig("ModelOutput/valfig.png")
 
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+    AUTOTUNE = tf.data.AUTOTUNE
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+    train_set = train_set.cache().shuffle(1600).prefetch(buffer_size=AUTOTUNE)
 
-epochs_range = range(epochs)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+    model = keras.Sequential([
+        layers.InputLayer((100,100,3)),
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.savefig("ModelOutput/history.png")
+        layers.Conv2D(256, 2, strides=(1, 1), activation= 'relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.2),
 
-validation_accuracy = model.evaluate(val_set)
+        layers.Conv2D(64, 2, strides=(1, 1), activation= 'relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.5),
 
-# Print the validation accuracy
-print("Validation Accuracy:", validation_accuracy)
+        layers.Conv2D(64, 4, activation= 'leaky_relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.35),
 
-print(model.evaluate(val_set, verbose = 1))
+        layers.Conv2D(64, 3, activation= 'relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.5),
+
+        layers.Conv2D(64, 3, activation= 'relu'),
+        layers.MaxPooling2D(),
+    
+
+        layers.Flatten(),
+        layers.Dense(256, activation='elu'),
+        layers.Dropout(0.5),
+
+        layers.Dense(4, activation='softmax')
+    ])
+
+    optimizer=keras.optimizers.Adam()
+    optimizer.learning_rate.assign(0.0005)
+
+    model.compile(optimizer=optimizer,
+                loss=tf.keras.losses.CategoricalCrossentropy(),
+                metrics=['accuracy'])
+
+    model.build((BATCH_SIZE, 100, 100, 3))
+
+    model.summary()
+
+    # reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy', factor=0.2, patience=5, min_lr=0.00001)
+
+    epochs = 45
+    history = model.fit(
+    train_set,
+    validation_data=val_set,
+    epochs=epochs,
+    #   callbacks = reduce_lr,
+    )
+
+    model.save("ModelOutput/modeled.keras")
+
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.savefig("ModelOutput/history.png")
+
+    validation_accuracy = model.evaluate(val_set)
+
+    # Print the validation accuracy
+    print("Validation Accuracy:", validation_accuracy)
+
+    predict_image(model, "Data/Train/Medium/Crack__20180419_05_06_26,199.bmp")
+    execute_predict()
+    
+
+def predict_image(model, image_path):
+    labels = ["Large", "Medium", "None", "Small"]
+    input_image = Image.open(image_path)
+    test_img = keras.utils.load_img(image_path, target_size=(100, 100))
+    img_array = keras.utils.img_to_array(test_img)
+    img_array = tf.expand_dims(img_array, 0) 
+    img_array = img_array/255.
+    predictions = model.predict(img_array)
+    for i,prob in enumerate(predictions[0]):
+        print(labels[i], ": ", prob)
+
+    # set_font = ImageFont.load_default()
+    # set_font.size = 100
+
+    set_font = ImageFont.truetype("UbuntuMono-R.ttf", size=100)
+    
+
+    draw_image = ImageDraw.Draw(input_image)
+    draw_image.text((1400,1700), "Large: " + str(round(predictions[0][0] * 100)), font = set_font, fill = "green")
+    draw_image.text((1400,1750), "Medium: " + str(round(predictions[0][1] * 100)), font = set_font, fill = "green")
+    draw_image.text((1400,1800), "Small: " + str(round(predictions[0][3] * 100)), font = set_font, fill = "green")
+    draw_image.text((1400,1850), "None: " + str(round(predictions[0][2] * 100)), font = set_font, fill = "green")
+    filename = image_path.split('/')[-1]
+
+    input_image.save("ModelOutput/Predictions/Pred_" + filename)
+
+
+
+def execute_predict():
+    loaded_model = keras.models.load_model("ModelOutput/modeled.keras")
+    predict_image(loaded_model, "Data/Test/Medium/Crack__20180419_06_19_09,915.bmp")
+    predict_image(loaded_model, "Data/Test/Large/Crack__20180419_13_29_14,846.bmp")
+    
+
+
+if __name__ == "__main__":
+    execute_predict()
+
